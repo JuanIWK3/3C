da4e4b75d1717e259f802210fc5e993f328f55d9
juaniwk3 <juaniwk3@gmail.com>
2022-07-10 16:57:56 -0300

    first commit

---
+++ src/main/java/control/max/ControleCobrancaCondominio/ControleCobrancaCondominioApplication.java
@@ -0,0 +1,13 @@
+package control.max.ControleCobrancaCondominio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControleCobrancaCondominioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControleCobrancaCondominioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/control/max/ControleCobrancaCondominio/ControleCobrancaCondominioApplicationTests.java
@@ -0,0 +1,13 @@
+package control.max.ControleCobrancaCondominio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControleCobrancaCondominioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
